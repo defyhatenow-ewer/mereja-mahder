7a26036fc11950615c3de97aa5790d2acfc0ee87
saisilinus <saisilinus.work@gmail.com>
2025-04-10 10:59:13 +0300

    fix: auth issue

--- src/collections/Categories.ts
+++ src/collections/Categories.ts
@@ -3,14 +3,14 @@ import type { CollectionConfig } from 'payload'
 import { slugField } from '@/fields/slug'
 import { isLoggedIn } from '@/access/isLoggedIn'
 import { isAdminOrEditor } from '@/access/isAdminOrEditor'
-import { authenticatedOrPublished } from '@/access/authenticatedOrPublished'
+import { anyone } from '@/access/anyone'
 
 export const Categories: CollectionConfig = {
   slug: 'categories',
   access: {
     create: isLoggedIn,
     delete: isAdminOrEditor,
-    read: authenticatedOrPublished,
+    read: anyone,
     update: isAdminOrEditor,
   },
   admin: {
